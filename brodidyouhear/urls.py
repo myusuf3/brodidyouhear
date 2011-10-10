8487db71bdc50c9b83e60626d84f86507b525d1a
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2011-10-10 00:14:53 -0400

    adding the about page

--- brodidyouhear/urls.py
+++ brodidyouhear/urls.py
@@ -12,6 +12,7 @@ handler500 = 'webcomic.views.server_error'
 urlpatterns = patterns('',
      url(r'^$', 'webcomic.views.index', name='index_comic'),
      url(r'^comic/(?P<id>\d+)/$', 'webcomic.views.comic', name='comic_view'),
+     url(r'^about/', 'webcomic.views.about', name='about'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
  	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- brodidyouhear/webcomic/views.py
+++ brodidyouhear/webcomic/views.py
@@ -50,6 +50,31 @@ def index(request):
 	return render(request, 'index.html', {'comic': comic, 'next_pk': next_pk, 'pre_pk': pre_pk, 'rand_pk': rand_pk, 
 										'tweet': tweet, 'tweet_url': tweet_url, 'comic_title_encode': comic_title_encode})
 
+def about(request):
+	#latest comic 
+	max_pk = Comic.objects.aggregate(max_pk=models.Max('pk'))['max_pk']
+	curr_pk = max_pk
+	next_pk = Comic.objects.filter(pk__gt=curr_pk).aggregate(next_pk=models.Min('pk'))['next_pk']
+	pre_pk = Comic.objects.filter(pk__lt=curr_pk).aggregate(pre_pk=models.Max('pk'))['pre_pk']
+	random_comic =  Comic.objects.order_by('?')[0]
+	rand_pk = random_comic.pk
+	comic = Comic.objects.get(pk=max_pk)
+
+	if next_pk is None:
+		#this handles the case where the user is at the last comic
+		next_pk = curr_pk
+
+	if pre_pk is None:
+		#this handles the case where the user at the first comic
+		pre_pk = 1
+
+	tweet = urlencode_tweet_text(comic.title)
+	tweet_url = urlencode_tweet_url(curr_pk)
+	comic_title_encode = urlencode_comic_title(comic.title)
+
+	return render(request, 'about.html',  {'comic': comic, 'next_pk': next_pk, 'pre_pk': pre_pk, 'rand_pk': rand_pk, 
+										'tweet': tweet, 'tweet_url': tweet_url, 'comic_title_encode': comic_title_encode})
+
 def comic(request, id):
 	comic = get_object_or_404(Comic, pk=id)
 	curr_pk = id
